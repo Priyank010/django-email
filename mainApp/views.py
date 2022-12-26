fcebe7aa489f795cbe7b9026d69384bb1fb2406a
Priyank Desai <priyank@finflock.com>
2022-12-26 12:57:31 +0530

    First commit

---
+++ mainApp/views.py
@@ -0,0 +1,33 @@
+from rest_framework.views import APIView
+from django.conf import settings
+from django.core.mail import send_mail
+from rest_framework.response import Response
+
+class EmailAPI(APIView):
+    def get(self, request):
+        subject = self.request.GET.get('subject')
+        txt_ = self.request.GET.get('text')
+        html_ = self.request.GET.get('html')
+        recipient_list = self.request.GET.get('recipient_list')
+        from_email = settings.DEFAULT_FROM_EMAIL
+
+        if subject is None and txt_ is None and html_ is None and recipient_list is None:
+            return Response({'msg': 'There must be a subject, a recipient list, and either HTML or Text.'}, status=200)
+        elif html_ is not None and txt_ is not None:
+            return Response({'msg': 'You can either use HTML or Text.'}, status=200)
+        elif html_ is None and txt_ is None:
+            return Response({'msg': 'Either HTML or Text is required.'}, status=200)
+        elif recipient_list is None:
+            return Response({'msg': 'Recipient List required.'}, status=200)
+        elif subject is None:
+            return Response({'msg': 'Subject required.'}, status=200)
+        else:
+            sent_mail = send_mail(
+                subject,
+                txt_,
+                from_email,
+                recipient_list.split(','),
+                html_message=html_,
+                fail_silently=False,
+            )
+            return Response({'msg': sent_mail}, status=200)
